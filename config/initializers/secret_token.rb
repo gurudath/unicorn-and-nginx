d526639785cacaf7638eca42646f718cb6512d7c
Gurudath Bn <gurudath.1989@gmail.com>
2014-05-17 19:28:06 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UniNgxRails::Application.config.secret_key_base = 'b86330b1f3d5dfa7b39605ac75d67329a93232cdc486a72ab0aa5210aa9102fbc0868877045fe92284d309b34c1f8bd6472a547f2f9e98d161bb8347cc6cafdd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UniNgxRails::Application.config.session_store :cookie_store, key: '_uni_ngx_rails_session'
